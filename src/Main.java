abee99b7109a5b4f33ce7043a03732cd096e2749
MatheusMoreira-dev <matheus.moreira.marcelino.dev@gmail.com>
2025-05-13 09:02:39 -0300

    Cliente | Requisição | Resposta

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String url = "";
+
+        //Cliente
+        HttpClient client = HttpClient.newHttpClient();
+
+        //Requisição
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url)).build();
+
+        //Resposta
+        HttpResponse<String> response = client.
+                send(request, HttpResponse.BodyHandlers.ofString());
     }
 }
